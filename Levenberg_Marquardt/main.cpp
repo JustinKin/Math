08113a31d4da208734004e0b17351626b2dbabe0
JustinKin <justinkin2020@outlook.com>
2021-01-22 17:57:02 +0800

    first

---
+++ Levenberg_Marquardt/main.cpp
@@ -0,0 +1,51 @@
+/*
+                           _ooOoo_
+                          o8888888o
+                          88" . "88
+                          (| -_- |)
+                          O\  =  /O
+                       ____/`---'\____
+                     .'  \\|     |//  `.
+                    /  \\|||  :  |||//  \
+                   /  _||||| -:- |||||-  \
+                   |   | \\\  -  /// |   |
+                   | \_|  ''\---/''  |   |
+                   \  .-\__  `-`  ___/-. /
+                 ___`. .'  /--.--\  `. . __
+              ."" '<  `.___\_<|>_/___.'  >'"".
+             | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+             \  \ `-.   \_ __\ /__ _/   .-` /  /
+        ======`-.____`-.___\_____/___.-`____.-'======
+                           `=---='
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+          一心向善	好运相伴	佛祖保佑	永无BUG
+*/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <map>
+#include <set>
+#include <utility>
+#include <memory>
+#include <cmath>
+#include "D:\FMT\include\fmt\format.h"
+#include "D:\FMT\include\fmt\ranges.h"
+#include "D:\FMT\include\fmt\os.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    fmt::print("{}\n", "Hello World!");
+
+    system("pause");
+    return 0;
+}
